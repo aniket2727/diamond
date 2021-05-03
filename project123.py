23c4c57b201e189b6263683ca5c9da52bedc028e
aniket2727 <80188510+aniket2727@users.noreply.github.com>
2021-05-03 14:12:27 +0530

    Add files via upload

---
+++ project123.py
@@ -0,0 +1,21 @@
+not1=input("enter the first nomini")
+not2=iput("enter the second nomini")
+nom1votes=0
+nom2votes=0
+votesid=[1,2,3,4,5,6,7,8,9,10]
+num_of_votes_id_len(votesid)
+while True:
+if votesid==[]:
+print("voting session is over")
+if nom1votes>nom2votes
+percent=(nom1votes/numberviters)*100
+print("nomini1,"has won","with",percent"%votes)
+elif nom2votes>nom1votes:
+print("nomini1,"has won","with",percent"%votes)
+elif vote==2:
+print("thank U for cashing the votes")
+  else:
+print("you are not the voter u already voted")
+while True:
+if votesid==[]:
+print("voting session is over
